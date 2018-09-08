c429ae00547553343745c35352ee91cc51e2762b
marc <marc@marcfischer.org>
2018-09-08 17:17:44 +0200

    ir receiver relevant code

---
+++ firmware/fantimote_atmelStudio/fantimote_hardwaretest/fantimote_hardwaretest/ir_receive.c
@@ -0,0 +1,6 @@
+/*
+ * ir_receive.c
+ *
+ * Created: 08.09.2018 16:30:52
+ *  Author: marc
+ */ 
---
+++ firmware/fantimote_atmelStudio/fantimote_hardwaretest/fantimote_hardwaretest/ir_receive.h
@@ -0,0 +1,16 @@
+/*
+ * ir_receive.h
+ *
+ * Created: 08.09.2018 16:31:15
+ *  Author: marc
+ */ 
+
+
+#ifndef IR_RECEIVE_H_
+#define IR_RECEIVE_H_
+
+
+
+
+
+#endif /* IR_RECEIVE_H_ */
