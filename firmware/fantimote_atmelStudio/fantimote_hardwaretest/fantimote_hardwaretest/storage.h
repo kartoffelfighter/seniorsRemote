96b36676d33442fa81ffd4fb6a0b2f58a688b74e
marc <marc@marcfischer.org>
2018-09-08 17:18:04 +0200

    hard coded storage conditions

---
+++ firmware/fantimote_atmelStudio/fantimote_hardwaretest/fantimote_hardwaretest/storage.h
@@ -0,0 +1,15 @@
+/*
+ * storage.h
+ *
+ * Created: 01.09.2018 15:25:42
+ *  Author: marc
+ * 
+ * Function:
+ * Storage description
+ */ 
+
+
+#ifndef STORAGE_H_
+#define STORAGE_H_
+
+#endif /* STORAGE_H_ */
